5428cfea88022336e46ad2064d0abb221e9e2230
Cyber Walrus <stonewalrusfan@gmail.com>
2019-03-29 13:47:09 +0300

    Hello World page

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+
+const outputDirectory = `dist`;
+
+module.exports = {
+  entry: `./client/app/index.js`,
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: `bundle.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [`style-loader`, `css-loader`]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: `url-loader?limit=100000`
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": `http://localhost:8080`
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./client/public/index.html`,
+      favicon: `./client/public/favicon.ico`
+    })
+  ]
+};
